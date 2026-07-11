09d84b5be5b6b7faa624613376125a62c4e2eccf
taqyeem-debug <ahmedtaqyeem04@gmail.com>
2026-07-11 07:41:48 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -89,9 +89,13 @@ function AnimatedRoutes() {
   );
 }
 
+const routerBasename = import.meta.env.BASE_URL === '/'
+  ? undefined
+  : import.meta.env.BASE_URL.replace(/\/$/, '');
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <AnimatedRoutes />
     </BrowserRouter>
   );
